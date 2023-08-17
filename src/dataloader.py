a8de6a01ef89beb2f38e0a8d7aa3857cd7358150
HamzaGbada <gbadahamza18@gmail.com>
2023-08-17 16:10:34 +0100

    [INIT] dataloader

---
+++ src/dataloader.py
@@ -0,0 +1,24 @@
+
+class dataloader_normal:
+    """ Simple dataloader which return original images"""
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+class dataloader_binairy_image:
+    """ Simple dataloader which return binary images"""
+
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
