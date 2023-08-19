e4cd4e8629c113de1de07afa6d8d6ee243297871
yasahiro10 <yassineouardani381@gmail.com>
2023-08-19 16:12:47 +0100

    [update] dataloader_normal using the appropriate format

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,36 +2,43 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 import pandas as pd
-
+from torchvision import transforms
 from src.utils.setup_logger import logger
-
+from PIL import Image
 
 class dataloader_normal():
     """ Simple dataloader which return original images"""
     def __init__(self):
-        xy = pd.read_csv('C:/Users/msi/Desktop/arabian calligraphy dataset/train/annotations.csv', delimiter=",")
-        self.x = xy.iloc[:, 0].tolist()# Assuming the first column is for x data
-        self.y = xy.iloc[:, 1:].values # Convert DataFrame to NumPy array because the other columns
+        image_tensor=[]
+        xy = pd.read_csv('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/annotations.csv', delimiter=",")
+        L= xy.iloc[:, 0].tolist()# Assuming the first column is for x data
+        for i in L:
+            image=Image.open('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/{}'.format(i))
+            to_tensor=transforms.ToTensor()
+            image_tensor.append(to_tensor(image))
+        self.x=image_tensor
+        self.y=xy.iloc[:,3]
+        self.z = xy.iloc[:, 4:].values # Convert DataFrame to NumPy array because the other columns
         self.n_samples = xy.shape[0]
 
     def __getitem__(self, index):
         x_item = self.x[index]
         y_item = self.y[index]
+        z_item = self.z[index]
         logger.debug(x_item)
         logger.debug(y_item)
-        return x_item, y_item
+        return x_item, y_item,z_item
 
     def __len__(self):
         return self.n_samples
 
 
 
-class dataloader_binairy_image:
+class dataloader_binairy_image():
     """ Simple dataloader which return binary images"""
 
     def __init__(self):
         pass
-
     def __getitem__(self, item):
         pass
 
