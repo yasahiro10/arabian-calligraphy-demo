30659892495dce167a07f167091987dc0c5789ca
yasahiro10 <yassineouardani381@gmail.com>
2023-08-21 13:45:27 +0100

    [update] dataloader_normal using the appropriate format

--- src/dataloader.py
+++ src/dataloader.py
@@ -5,20 +5,24 @@ import pandas as pd
 from torchvision import transforms
 from src.utils.setup_logger import logger
 from PIL import Image
+import cv2
+
 
 class dataloader_normal():
     """ Simple dataloader which return original images"""
+
     def __init__(self):
-        image_tensor=[]
-        xy = pd.read_csv('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/annotations.csv', delimiter=",")
-        L= xy.iloc[:, 0].tolist()# Assuming the first column is for x data
+        image_tensor = []
+        xy = pd.read_csv('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/annotations.csv',
+                         delimiter=",")
+        L = xy.iloc[:, 0].tolist()  # Assuming the first column is for x data
         for i in L:
-            image=Image.open('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/{}'.format(i))
-            to_tensor=transforms.ToTensor()
+            image = Image.open('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/{}'.format(i))
+            to_tensor = transforms.ToTensor()
             image_tensor.append(to_tensor(image))
-        self.x=image_tensor
-        self.y=xy.iloc[:,3]
-        self.z = xy.iloc[:, 4:].values # Convert DataFrame to NumPy array because the other columns
+        self.x = image_tensor
+        self.y = xy.iloc[:, 3]
+        self.z = xy.iloc[:, 4:].values  # Convert DataFrame to NumPy array because the other columns
         self.n_samples = xy.shape[0]
 
     def __getitem__(self, index):
@@ -27,21 +31,31 @@ class dataloader_normal():
         z_item = self.z[index]
         logger.debug(x_item)
         logger.debug(y_item)
-        return x_item, y_item,z_item
+        return x_item, y_item, z_item
 
     def __len__(self):
         return self.n_samples
 
 
-
-class dataloader_binairy_image():
+class dataloader_binairy():
     """ Simple dataloader which return binary images"""
-
     def __init__(self):
-        pass
+        image_tensor = []
+        xy = pd.read_csv('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/annotations.csv',
+                         delimiter=",")
+        L = xy.iloc[:, 0].tolist()  # Assuming the first column is for x data
+        for i in L:
+            image = Image.open('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/{}'.format(i))
+            image = cv2.imread('C:/Users/msi/PycharmProjects/arabian-calligraphy-demo/data/train/{}'.format(i),
+                               cv2.IMREAD_GRAYSCALE)
+            binary_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            to_tensor = transforms.ToTensor()
+            image_tensor.append(to_tensor(binary_image))
+        self.x = image_tensor
+        self.n_samples = xy.shape[0]
     def __getitem__(self, item):
-        pass
+        x_item = self.x[index]
+        return x_item
 
     def __len__(self):
-        pass
-
+        return self.n_samples
--- unit_test.py
+++ unit_test.py
@@ -7,11 +7,13 @@ from src.utils.setup_logger import logger
 class TestDataLoader(unittest.TestCase):
 
     def test_binary(self):
-        pass
-
-    def test_dataloader_length(self):
-
-        dataset = dataloader_normal()
+        dataset = dataloader_binairy()
         first_data = dataset[0]
         logger.debug(first_data)
-        print(dataset[0][0].size())
+
+    #def test_dataloader_length(self):
+
+       # dataset = dataloader_normal()
+       # first_data = dataset[0]
+        #logger.debug(first_data)
+
