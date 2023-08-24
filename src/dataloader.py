a8c2aefd391792d5e390a3db57a59bb546b37c4d
yasahiro10 <yassineouardani381@gmail.com>
2023-08-24 19:32:57 +0100

    [Update] ToDo list done and test is verified for dataloader_binary

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,59 +4,66 @@ import pandas as pd
 from PIL import Image
 from torchvision import transforms
 
-# TODO: optimize imports done ctrl+Alt+o done
 class dataloader_normal():
     """ Simple dataloader which return original images"""
 
     def __init__(self):
         image_tensor = []
         to_tensor = transforms.ToTensor()
-        calligraphy_data= pd.read_csv('data/train/_annotations.csv', delimiter=",")
+        calligraphy_data= pd.read_csv('data/train/annotations.csv', delimiter=",")
         for index,row in calligraphy_data.iterrows():
             image_path=row['filename']
             xmin, ymin, xmax, ymax = row['xmin'], row['ymin'], row['xmax'], row['ymax']
             image = Image.open('data/train/{}'.format(image_path))
             cropped_image = image.crop((xmin, ymin, xmax, ymax))
-            # TODO: apply transopose before putting image into list
-            image_tensor.append(to_tensor(cropped_image))
+            # TODO: apply transopose before putting image into list DONE
+            image_tensor.append(to_tensor(cropped_image).numpy().transpose(1, 2, 0))
 
         self.data = {
             "cropped_bbox": image_tensor,
-            "bbox": calligraphy_data.iloc[:, 4:].values,
-            "label": calligraphy_data.iloc[:, 3]
+            "bbox": calligraphy_data.iloc[:,4:].values,
+            "label": calligraphy_data.iloc[:,3]
         }
 
-
-
+    def __repr__(self):
+        return repr(self.data)
     def __getitem__(self, index):
-        return  self.data["cropped_bbox"][index],self.data["bbox"][index],self.data["label"][index]
+        return self.data[index]
 
     def __len__(self):
         return len(self.data["label"])
-
 
 class dataloader_binairy():
     """ Simple dataloader which return binary images"""
     def __init__(self):
         image_tensor = []
         to_tensor = transforms.ToTensor()
-        xy = pd.read_csv('data/train/annotations.csv',
+        calligraphy_data= pd.read_csv('data/train/annotations.csv',
                          delimiter=",")
-        L = xy.iloc[:, 0].tolist()  # Assuming the first column is for x data
-        for i in L:
-            image_jpeg = Image.open('data/train/{}'.format(i))
-            image_np = np.array(image_jpeg)
+        L = calligraphy_data.iloc[:, 0].tolist()  # Assuming the first column is for x data
+        for index, row in calligraphy_data.iterrows():
+            image_path = row['filename']
+            xmin, ymin, xmax, ymax = row['xmin'], row['ymin'], row['xmax'], row['ymax']
+            image_jpeg= Image.open('data/train/{}'.format(image_path))
+            cropped_image = image_jpeg.crop((xmin, ymin, xmax, ymax))
+            image_np = np.array(cropped_image)
             image_gray=cv2.cvtColor(image_np,cv2.COLOR_BGR2GRAY)
             # Convert the numpy array to a format compatible with OpenCV
             image = np.squeeze(image_gray)  # Remove any single-dimensional dimensions
             image = (image * 255).astype(np.uint8)
             _, binary_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-            image_tensor.append(to_tensor(binary_image))
-        self.x = image_tensor
-        self.n_samples = xy.shape[0]
+            image_tensor.append(to_tensor(binary_image).numpy().transpose(1, 2, 0))
+        self.data = {
+            "cropped_bbox": image_tensor,
+            "bbox": calligraphy_data.iloc[:, 4:].values,
+            "label": calligraphy_data.iloc[:, 3]
+        }
+
+    def __repr__(self):
+        return repr(self.data)
+
     def __getitem__(self, index):
-        x_item = self.x[index]
-        return x_item
+        return self.data[index]
 
     def __len__(self):
-        return self.n_samples
+        return len(self.data["label"])
--- unit_test.py
+++ unit_test.py
@@ -1,23 +1,44 @@
 import unittest
 from matplotlib import pyplot as plt
 import pandas as pd
-from src.dataloader import dataloader_normal, dataloader_binairy
+from src.dataloader import dataloader_normal,dataloader_binairy
 from src.utils.setup_logger import logger
 
 
 class TestDataLoader(unittest.TestCase):
 
     def test_length_annotation(self):
-        annotation = pd.read_csv("data/train/_annotations.csv")
+        # TODO: dataset should contains something like that: returned by dataset
+        #       { "cropped_bbox" : [tensors...],
+        #         "bbox":  [[x,y,w,h]...],
+        #         "label":[label1,..]
+        #       }
+        annotation = pd.read_csv("data/train/annotations.csv")
         dataset = dataloader_normal()
-        self.assertEqual(annotation.shape[0], len(dataset.data['cropped_bbox']))
-        self.assertEqual(annotation.shape[0], len(dataset.data['bbox']))
-        self.assertEqual(annotation.shape[0], len(dataset.data['label']))
+        logger.debug(dataset["label"][0])
 
-    def test_binary(self):
-        # TODO: Apply same test for binary image and Apply test if it's a binary image or not
-        dataset = dataloader_binairy()
-        first_data = dataset[1]
-        logger.debug(first_data)
-        plt.imshow(first_data[0].numpy(), cmap='gray')
+        plt.imshow(dataset["cropped_bbox"][0])
         plt.show()
+        # logger.debug(dataset["label"])
+        # self.assertEqual(annotation.shape[0], len(dataset["cropped_bbox"]))
+        # self.assertEqual(annotation.shape[0], len(dataset["bbox"]))
+        # self.assertEqual(annotation.shape[0], len(dataset["label"]))
+    def test_binary(self):
+        annotation = pd.read_csv("data/train/annotations.csv")
+        dataset = dataloader_binairy()
+        # logger.debug(dataset)
+        # self.assertEqual(annotation.shape[0], len(dataset["cropped_bbox"]))
+        # self.assertEqual(annotation.shape[0], len(dataset["bbox"]))
+        # self.assertEqual(annotation.shape[0], len(dataset["label"]))
+        plt.imshow(dataset["cropped_bbox"][0],cmap='gray')
+        plt.show()
+
+    def test_dataloader_length(self):
+
+        dataset = dataloader_normal()
+        first_data = dataset[0]
+        logger.debug(first_data)
+        logger.debug(first_data[0].numpy().shape)
+        plt.imshow(first_data[0].numpy().transpose(1, 2, 0))
+        plt.show()
+
