8e337482c3fb23d01fdc4b7fd396e4611da9f6fa
HannachiCh <chaimahanechi4@gmail.com>
2023-08-25 14:12:23 +0200

    [UPDATE] unit_test

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,15 +1,54 @@
+import torch
+import unittest
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+import logging
 
-class dataloader_normal:
-    """ Simple dataloader which return original images"""
-    def __init__(self):
-        pass
+logging.basicConfig(level=logging.DEBUG)
 
-    def __getitem__(self, item):
-        pass
+class dataloader_normal(Dataset):
+    def __init__(self, data_file, images_folder):
+        self.data = self.load_data(data_file)
+        self.images_folder = images_folder
+
+    def __getitem__(self, index):
+        image_filename = self.data['images'][index]
+        bbox_list = self.data['bbox'][index]
+        label = self.data['label'][index]
+
+        image = self.load_image(image_filename)
+        cropped_images = [self.crop_image(image, box) for box in bbox_list]
+        bbox_tensors = [torch.tensor(box, dtype=torch.float32) for box in bbox_list]
+
+        return {'images': cropped_images, 'bbox': bbox_tensors, 'label': label}
 
     def __len__(self):
-        pass
+        return len(self.data['images'])
 
+    def load_data(self, data_file):
+        data = {'images': [], 'bbox': [], 'label': []}
+        with open(data_file, 'r') as f:
+            lines = f.readlines()[1:]
+            for line in lines:
+                parts = line.strip().split(',')
+                filename = parts[0]
+                bbox = list(map(int, parts[4:8]))
+                label = parts[3]
+                data['images'].append(filename)
+                data['bbox'].append(bbox)
+                data['label'].append(label)
+        return data
+
+    def load_image(self, filename):
+        image_path = f'C:/Users/ACER/Desktop/Stage/code/arabian-calligraphy-demo/Data/_annotations.csv'
+        image = Image.open(image_path).convert('RGB')
+        return transforms.ToTensor()(image)
+
+    def crop_image(self, image, bbox):
+        xmin, ymin, xmax, ymax = bbox
+        cropped_image = image[:, ymin:ymax, xmin:xmax]
+        return cropped_image
 class dataloader_binairy_image:
     """ Simple dataloader which return binary images"""
 
@@ -21,4 +60,3 @@ class dataloader_binairy_image:
 
     def __len__(self):
         pass
-
--- unit_test.py
+++ unit_test.py
@@ -1,10 +1,38 @@
 import unittest
+import logging
+from dataloader_normal import dataloader_normal  
 
+logging.basicConfig(level=logging.DEBUG)
 
 class TestDataLoader(unittest.TestCase):
 
     def test_binary(self):
-        pass
+        pass 
+    
+    def test_length_annotation(self):
+        data_file = 'C:/Users/ACER/Desktop/Stage/code/arabian-calligraphy-demo/Data/_annotations.csv'  
+        images_folder = ':/Users/ACER/Desktop/Stage/code/arabian-calligraphy-demo/Data'  
+
+        dataset = dataloader_normal(data_file, images_folder)
+        data_length = len(dataset)
+
+        annotations = dataset.load_data(data_file)['images']
+        annotations_length = len(annotations)
+
+        logging.debug(f"Data length: {data_length}, Annotations length: {annotations_length}")
+        self.assertEqual(data_length, annotations_length)
 
     def test_dataloader_length(self):
+        
+        data_file = 'C:/Users/ACER/Desktop/Stage/code/arabian-calligraphy-demo/Data/_annotations.csv'  
+        images_folder = 'p:/Users/ACER/Desktop/Stage/code/arabian-calligraphy-demo/Data'  
+
+        dataset = dataloader_normal(data_file, images_folder)
+
+        expected_length = len(dataset.load_data(data_file)['images'])
+        actual_length = len(dataset)
+
+        logging.debug(f"Expected length: {expected_length}, Actual length: {actual_length}")
+        self.assertEqual(actual_length, expected_length)
+
         pass
