[FIX ISSUE]F1-score, precision, and recall for train and validation.  plot for the previous metrics with the loss

src/models.py
@@ -1,5 +1,5 @@
 from torch import nn
-from torch.nn import init
+
 from src.utils.setup_logger import logger
 from torchsummary import summary
 class VGG16(nn.Module):
@@ -69,12 +69,12 @@ class VGG16(nn.Module):
                 nn.MaxPool2d(kernel_size=2, stride=2))
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),
-            nn.LazyLinear(4096),
+            nn.LazyLinear(512),
             nn.ReLU()
         )
         self.fc2 = nn.Sequential(
             nn.Dropout(0.5),
-            nn.LazyLinear(4096),
+            nn.LazyLinear(412),
             nn.ReLU()
         )
         self.fc3 = nn.Sequential(
@@ -93,10 +93,10 @@ class VGG16(nn.Module):
         out = self.layer7(out)
         out = self.layer8(out)
         out = self.layer9(out)
-        out = self.layer10(out)
-        out = self.layer11(out)
-        out = self.layer12(out)
-        out = self.layer13(out)
+        #out = self.layer10(out)
+        #out = self.layer11(out)
+        #out = self.layer12(out)
+        #out = self.layer13(out)
         logger.debug(f"out.shape {out.shape}")
         out = out.reshape(out.size(0), -1)
         logger.debug(f"out.shape {out.shape}")
@@ -172,9 +172,6 @@ class VGg16(nn.Module):
         self.classifier = nn.Sequential(
             nn.Dropout(0.5),
             nn.LazyLinear(256),
-#            nn.ReLU(),
-#            nn.Dropout(0.5),
-#            nn.LazyLinear(4096),
             nn.ReLU(),
             nn.LazyLinear(num_classes))
 

train.py
@@ -1,42 +1,35 @@
 import torch
 from torch import nn, optim
-from torch.utils.data import DataLoader
-
-from src.models import VGG16
-from src.dataloader import dataloader_augmented, dataloader_normal
+from torch.utils.data import DataLoader, random_split
+from src.dataloader import dataloader_normal
 from src.utils.setup_logger import logger
-
-# num_classes = 18
-# num_epochs = 20
-# batch_size = 16
-# learning_rate = 0.005
-#
-# model = VGG16(num_classes)
-#
-# criterion = nn.CrossEntropyLoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.005, momentum = 0.9)
-# dataset = dataloader_augmented(["Rotation","Gaussian blur","ColorJitter","GaussianNoise"],0,23,(106.67, 106.67),(2,2),4,1,0.1)
-# total_step= len(dataset.data["cropped_bbox"])
-# for epoch in range(num_epochs):
-#     for i in range(len(dataset.data["cropped_bbox"])):
-#         images = dataset.data["cropped_bbox"][i]
-#         labels = dataset.data["label"][i]
-#         outputs = model(images)
-#         loss = criterion(outputs, labels)
-#
-#         # Backward and optimize
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#     print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
-#
+from sklearn.metrics import f1_score, precision_score, recall_score
+import matplotlib.pyplot as plt
 
 
+def train(model, epochs = 10, batch_size = 1,validation_split = 0.2):
+    train_losses = []
+    train_f1_scores = []
+    train_precisions = []
+    train_recalls = []
 
-def train(model, epochs = 10, batch_size = 1):
-    dataloader = DataLoader(dataloader_normal(), batch_size=batch_size, shuffle=True)
+    validation_losses = []
+    validation_f1_scores = []
+    validation_precisions = []
+    validation_recalls = []
+    dataloader= dataloader_normal()
 
+    # Calculer le nombre d'échantillons pour l'ensemble de validation
+    num_samples = len(dataloader)
+    num_validation = int(validation_split * num_samples)
+    num_train = num_samples - num_validation
+
+    # Diviser les données en ensembles d'entraînement et de validation
+    train_dataset, validation_dataset = random_split(dataloader ,[num_train, num_validation])
+
+    # Créer des DataLoaders pour les ensembles d'entraînement et de validation
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size)
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
@@ -48,8 +41,8 @@ def train(model, epochs = 10, batch_size = 1):
     for epoch in range(epochs):
         running_loss = 0.0
 
-        for i, data in enumerate(dataloader):
-            inputs, bbox, labels = data
+        for i, data in enumerate(train_dataloader):
+            inputs, bbx, labels = data
 
             # Move data to the appropriate device
             inputs = inputs.permute(0, 3, 1, 2)
@@ -59,7 +52,6 @@ def train(model, epochs = 10, batch_size = 1):
                              "ya": 17}
             labels = [label_mapping[label_str] for label_str in labels]
             logger.debug(labels)
-            #labels = torch.tensor(labels)
             labels = torch.tensor(labels).to(device)
             # Zero the parameter gradients
             optimizer.zero_grad()
@@ -77,20 +69,107 @@ def train(model, epochs = 10, batch_size = 1):
             if i % 100 == 99:  # Print every 100 mini-batches
                 logger.debug(f"[Epoch {epoch + 1}, Batch {i + 1}] Loss: {running_loss / 100:.4f}")
                 running_loss = 0.0
+        train_loss = running_loss / len(train_dataloader)
+        train_losses.append(train_loss)
+
+        train_predicted = []  # Remplir avec les prédictions du modèle
+        train_ground_truth = []  # Remplir avec les étiquettes réelles
+
+        for data in train_dataloader:
+            inputs, _, labels = data
+            inputs = inputs.permute(0, 3, 1, 2).to(device)
+            labels = [label_mapping[label_str] for label_str in labels]
+            labels = torch.tensor(labels).to(device)
+            outputs = model(inputs)
+            predicted = torch.argmax(outputs, dim=1).tolist()
+            train_predicted.extend(predicted)
+            train_ground_truth.extend(labels.tolist())
+
+        train_f1 = f1_score(train_ground_truth, train_predicted, average='weighted')
+        train_prec = precision_score(train_ground_truth, train_predicted, average='weighted')
+        train_recall = recall_score(train_ground_truth, train_predicted, average='weighted')
+
+        train_f1_scores.append(train_f1)
+        train_precisions.append(train_prec)
+        train_recalls.append(train_recall)
 
     logger.debug("Training finished")
 
-    # # Validation
-    # with torch.no_grad():
-    #     correct = 0
-    #     total = 0
-    #     for images, labels in valid_loader:
-    #         images = images.to(device)
-    #         labels = labels.to(device)
-    #         outputs = model(images)
-    #         _, predicted = torch.max(outputs.data, 1)
-    #         total += labels.size(0)
-    #         correct += (predicted == labels).sum().item()
-    #         del images, labels, outputs
-    #
-    #     print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for i, data in enumerate(validation_dataloader):
+            validation_loss = running_loss / len(validation_dataloader)
+            validation_losses.append(validation_loss)
+
+            validation_predicted = []  # Remplir avec les prédictions du modèle
+            validation_ground_truth = []
+            inputs, bbx, labels = data
+            inputs = inputs.permute(0, 3, 1, 2)
+            inputs = inputs.to(device)
+            label_mapping = {"AIN": 0, "alph": 1, "baa": 2, "daa": 3, "faa": 4, "ha": 5, "Haa": 6, "Kaf": 7, "La": 8,
+                             "Meme": 9, "Noun": 10, "Qua": 11, "Raa": 12, "sad": 13, "sin": 14, "Ta": 15, "waw": 16,
+                             "ya": 17}
+            labels = [label_mapping[label_str] for label_str in labels]
+            logger.debug(labels)
+            labels = torch.tensor(labels).to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del inputs, labels, outputs
+        for data in validation_dataloader:
+            inputs, _, labels = data
+            inputs = inputs.permute(0, 3, 1, 2).to(device)
+            labels = [label_mapping[label_str] for label_str in labels]
+            labels = torch.tensor(labels).to(device)
+            outputs = model(inputs)
+            predicted = torch.argmax(outputs, dim=1).tolist()
+            validation_predicted.extend(predicted)
+            validation_ground_truth.extend(labels.tolist())
+
+        validation_f1 = f1_score(validation_ground_truth, validation_predicted, average='weighted')
+        validation_prec = precision_score(validation_ground_truth, validation_predicted, average='weighted',zero_division=0)
+        validation_recall = recall_score(validation_ground_truth, validation_predicted, average='weighted',zero_division=0)
+
+        validation_f1_scores.append(validation_f1)
+        validation_precisions.append(validation_prec)
+        validation_recalls.append(validation_recall)
+        plt.figure(figsize=(12, 6))
+
+        # Perte (Loss)
+        plt.subplot(2, 2, 1)
+        plt.plot(train_losses, label='Entraînement')
+        plt.plot(validation_losses, label='Validation')
+        plt.xlabel('Époque')
+        plt.ylabel('Perte (Loss)')
+        plt.legend()
+
+        # F1-score
+        plt.subplot(2, 2, 2)
+        plt.plot(train_f1_scores, label='Entraînement')
+        plt.plot(validation_f1_scores, label='Validation')
+        plt.xlabel('Époque')
+        plt.ylabel('F1-score')
+        plt.legend()
+
+        # Précision (Precision)
+        plt.subplot(2, 2, 3)
+        plt.plot(train_precisions, label='Entraînement')
+        plt.plot(validation_precisions, label='Validation')
+        plt.xlabel('Époque')
+        plt.ylabel('Précision (Precision)')
+        plt.legend()
+
+        # Rappel (Recall)
+        plt.subplot(2, 2, 4)
+        plt.plot(train_recalls, label='Entraînement')
+        plt.plot(validation_recalls, label='Validation')
+        plt.xlabel('Époque')
+        plt.ylabel('Rappel (Recall)')
+        plt.legend()
+
+        plt.tight_layout()
+        plt.show()
+
+    print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))